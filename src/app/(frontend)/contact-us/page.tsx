2de288d0e1afb505da77f7c860ffa522b7f60e49
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2025-02-06 04:10:46 +0100

    fix: increase the height of the contact-us form

--- src/app/(frontend)/contact-us/page.tsx
+++ src/app/(frontend)/contact-us/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <>
       <div className="bg-primary/50 w-full h-[116.83px]" />
-      <iframe src="/contact/index.html" width={'100%'} height={'1400px'} />
+      <iframe src="/contact/index.html" width={'100%'} height={'1420px'} />
     </>
   )
 }
