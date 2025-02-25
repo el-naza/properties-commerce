a23a58b6f83ef4c40d57817d07d050f05ba3c0df
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2025-02-25 06:16:07 +0100

    fix: split the domains for the cors setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -87,7 +87,7 @@ export default buildConfig({
     Inquiries,
     // TourSchedules,
   ],
-  cors: [getServerSideURL(), ...process.env.DOMAINS!].filter(Boolean),
+  cors: [getServerSideURL(), ...process.env.DOMAINS!.split(',')].filter(Boolean),
   email: resendAdapter({
     defaultFromAddress: process.env.EMAIL_USER!,
     defaultFromName: 'Vastel Credence',
